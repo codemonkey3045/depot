1fd0f30efa0dc9dd7fc0454e3623afb3aabdc2a9
codemonkey3045 <catia3045@gmail.com>
2014-01-22 20:35:03 -0500

    bug fix

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -13,22 +13,23 @@ class ProductTest < ActiveSupport::TestCase
 	end
 
 	test "product price must be positive" do
-		product = Product.new(title: 		"My Book",
-							  description:  "awesome book",
-							  image_url: 	"zzz.jpg")
-		product.price = -1
-		assert product.invalid?
-		assert_equal ["must be greater than or equal to 0.01"], 
-			product.errors[:price]
+	    product = Product.new(title:       "My Book Title",
+	                          description: "yyy",
+	                          image_url:   "zzz.jpg")
+	    product.price = -1
+	    assert product.invalid?
+	    assert_equal ["must be greater than or equal to 0.01"],
+	      product.errors[:price]
 
-		product.price = 0
-		assert product.invalid?
-		assert_equal ["must be greater than or equal to 0.01"],
-			product.errors[:price]
+	    product.price = 0
+	    assert product.invalid?
+	    assert_equal ["must be greater than or equal to 0.01"], 
+	      product.errors[:price]
 
-		product.price = 1
-		assert product.valid?
-	end
+	    product.price = 1
+	    assert product.valid?
+	  end
+
 
 	def new_product(image_url)
 		Product.new(title: 			"My Book Title",
