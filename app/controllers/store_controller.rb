9cd272d330b785cceeb56593c9dc36cdf70a0816
codemonkey3045 <catia3045@gmail.com>
2014-01-22 19:22:07 -0500

    Catalog page counter functional

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,20 @@
 class StoreController < ApplicationController
+
   def index
+  	@count = increment_counter
+  	@display_count = display_count?
     @products = Product.order(:title)
   end
+
+
+  protected 
+
+	def increment_counter
+		session[:counter] ||= 0
+		count = session[:counter] += 1
+	end
+
+	def display_count?
+		@count > 5 ? true : false
+	end
 end
