374b1798f73dfa3d1ff39497b3c880b09314bbe3
codemonkey3045 <catia3045@gmail.com>
2014-01-29 14:49:46 -0500

    basic email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
+
+# TO-DO: config for gmail later
+Depot::Application.configure do
+	config.action_mailer.delivery_method = :smtp
+end
+
