fad1808c629f810674917c14dfeaf620bb00aa7b
codemonkey3045 <catia3045@gmail.com>
2014-01-29 23:44:32 -0500

    basic login system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+    # ...
 
-  helper_method :timestamp
+  protected
 
-  def timestamp
-  	return Time.now.to_formatted_s(:long) 
-  end
-
-
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
 end
